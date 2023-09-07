c350608469a177a6744e6b6d1d161f8627612496
norhan2030 <105121297+norhan2030@users.noreply.github.com>
2023-09-07 18:48:24 +0300

    Add files via upload

---
+++ js/landing.js
@@ -0,0 +1,32 @@
+// let icon=document.querySelector("#ico");
+// let list=document.querySelector("#list");
+
+
+$("#ico").click(function(){
+    $(".hhhh").show(500);
+    // $("#list").hide(500);
+
+})
+
+
+// console.log(list);
+
+
+// icon.addEventListener("click",()=>
+// {
+//    console.log(list);
+//     list.classList.toggle('open') ;
+
+// })
+// icon.addEventListener("click",function()
+// {
+   
+//     // list.style.display="none";
+
+// })
+// icon.addEventListener("mouseover",function()
+// {
+//     console.log("aaaa");
+//     // list.style.display="block";
+
+// })
